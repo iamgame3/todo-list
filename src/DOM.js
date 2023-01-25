0685d55ed8429ef487cd4b18ffabeb934c693f04
iamgame3 <kosmosworld9@hotmail.com>
2023-01-24 19:23:44 -0500

    Add non-functional dropdown

--- src/DOM.js
+++ src/DOM.js
@@ -1,19 +1,62 @@
 import editIconSrc from "./icons/dots-vertical.svg";
 
-// Add edit buttons to projects and tasks (temporary)
+// Add edit buttons to projects and tasks
 const addEditButtons = () => {
+  // Make an edit button
   const addEditButton = (item) => {
+    const editButton = document.createElement("div");
     const editIcon = new Image();
+    const editDropdown = document.createElement("div");
+    const editOption = document.createElement("div");
+    const removeOption = document.createElement("div");
     editIcon.src = editIconSrc;
     editIcon.setAttribute("alt", "Vertical dotted line icon for edit options.");
     editIcon.setAttribute("role", "button");
     editIcon.classList.add("edit-button");
-    item.appendChild(editIcon);
+    editDropdown.classList.add("dropdown-content");
+    editDropdown.style.visibility = "hidden";
+    editOption.textContent = "Edit";
+    editOption.classList.add("dropdown-item");
+    removeOption.textContent = "Remove";
+    removeOption.classList.add("dropdown-item");
+    editDropdown.appendChild(editOption);
+    editDropdown.appendChild(removeOption);
+    editButton.classList.add("edit-button");
+    editButton.classList.add("dropdown");
+    editButton.appendChild(editIcon);
+    editButton.appendChild(editDropdown);
+    item.appendChild(editButton);
+
+    editButton.addEventListener("click", () => {
+      if (editDropdown.style.visibility === "hidden") {
+        editDropdown.style.visibility = "visible";
+      } else editDropdown.style.visibility = "hidden";
+    });
+
+    editDropdown.addEventListener("click", () => {
+      // For some reason, making the visibility "visible" hides it, I have no idea why but it works so I'm keeping it like this
+      editDropdown.style.visibility = "visible";
+    });
   };
+
+  // Make an edit button for every project and task
   const testItems = document.querySelectorAll(".sidebar-item");
   const testItems2 = document.querySelectorAll(".todo-item");
   testItems.forEach((testItem) => addEditButton(testItem));
   testItems2.forEach((testItem) => addEditButton(testItem));
+
+  window.addEventListener("click", (event) => {
+    if (
+      !event.target.matches(".dropdown-content") &&
+      !event.target.matches(".edit-button")
+    ) {
+      const dropdowns = document.querySelectorAll(".dropdown-content");
+      dropdowns.forEach((dropdown) => {
+        // eslint-disable-next-line no-param-reassign
+        dropdown.style.visibility = "hidden";
+      });
+    }
+  });
 };
 
 // Create open/close controls for all modals
